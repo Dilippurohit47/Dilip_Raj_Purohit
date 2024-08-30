b798ca63626193130c0933a0f34fe7343f5d6cfa
Dilippurohit47 <dilippurohit204@gmail.com>
2024-08-30 11:32:08 +0530

    footer exp

--- src/components/sections/Footer.jsx
+++ src/components/sections/Footer.jsx
@@ -92,7 +92,6 @@ const Footer = () => {
         <Nav>
           <NavLink href="#About">About</NavLink>
           <NavLink href="#Skills">Skills</NavLink>
-          <NavLink href="#Experience">Experience</NavLink>
           <NavLink href="#Projects">Projects</NavLink>
           <NavLink href="#Education">Education</NavLink>
         </Nav>
